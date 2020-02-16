1afa382c41145eeef4230bc6046f4e88d3ba2585
Frank Werner-Krippendorf <mail@hb9fxq.ch>
2020-02-16 15:16:22 +0100

    SDR Angel

--- SetupSdrAngelUbuntu1804.sh
+++ SetupSdrAngelUbuntu1804.sh
@@ -78,72 +78,72 @@ update_and_configure_system(){
     apt-get install -y cmake
 }
 
-#update_and_configure_system
+update_and_configure_system
 
 #read -p "Press enter to continue" 
 
 # clone and build CM265cc
-#clone_and_cd cm256cc https://github.com/f4exb/cm256cc.git "f21e8bc1e9afdb0b28672743dcec111aec1d32d9"
-#cmake_and_ldconfig
+clone_and_cd cm256cc https://github.com/f4exb/cm256cc.git "f21e8bc1e9afdb0b28672743dcec111aec1d32d9"
+cmake_and_ldconfig
 
 # clone and build SerialDV
-#clone_and_cd serialDV https://github.com/f4exb/serialDV.git "v1.1.4"
-#cmake_and_ldconfig
+clone_and_cd serialDV https://github.com/f4exb/serialDV.git "v1.1.4"
+cmake_and_ldconfig
 
 # clone and build DSDcc
-#clone_and_cd DSDcc https://github.com/f4exb/dsdcc.git "v1.8.6"
-#cmake_and_ldconfig
+clone_and_cd DSDcc https://github.com/f4exb/dsdcc.git "v1.8.6"
+cmake_and_ldconfig
 
 # Codec2/FreeDV
-#apt_install_yes "libspeexdsp-dev libsamplerate0-dev"
-#clone_and_cd codec2 https://github.com/drowe67/codec2.git 76a20416d715ee06f8b36a9953506876689a3bd2
-#cmake_and_ldconfig
+apt_install_yes "libspeexdsp-dev libsamplerate0-dev"
+clone_and_cd codec2 https://github.com/drowe67/codec2.git 76a20416d715ee06f8b36a9953506876689a3bd2
+cmake_and_ldconfig
 
 # clone and build Airspy
-#clone_and_cd libairspy https://github.com/airspy/host.git "bfb667080936ca5c2d23b3282f5893931ec38d3f"
-#cmake_and_ldconfig
+clone_and_cd libairspy https://github.com/airspy/host.git "bfb667080936ca5c2d23b3282f5893931ec38d3f"
+cmake_and_ldconfig
 
 # clone and build SDRplay RSP1
-#clone_and_cd libmirisdr https://github.com/f4exb/libmirisdr-4.git
-#cmake_and_ldconfig
+clone_and_cd libmirisdr https://github.com/f4exb/libmirisdr-4.git
+cmake_and_ldconfig
 
 # clone and build RTL-SDR
-# clone_and_cd rtl-sdr https://github.com/osmocom/rtl-sdr "be1d1206bfb6e6c41f7d91b20b77e20f929fa6a7"
-# cmake_and_ldconfig
+clone_and_cd rtl-sdr https://github.com/osmocom/rtl-sdr "be1d1206bfb6e6c41f7d91b20b77e20f929fa6a7"
+cmake_and_ldconfig
 
 # clone and build Pluto SDR
-#clone_and_cd libiio https://github.com/analogdevicesinc/libiio.git "826563e41b5ce9890b75506f672017de8d76d52d"
-#cmake_and_ldconfig
+clone_and_cd libiio https://github.com/analogdevicesinc/libiio.git "826563e41b5ce9890b75506f672017de8d76d52d"
+cmake_and_ldconfig
 
 # clone and build BladeRF all versions
-#clone_and_cd bladeRF https://github.com/Nuand/bladeRF.git "2019.07"
-#cmake_and_ldconfig
+clone_and_cd bladeRF https://github.com/Nuand/bladeRF.git "2019.07"
+cmake_and_ldconfig
 
 # clone and build HackRF
-#clone_and_cd hackrf/host https://github.com/mossmann/hackrf.git "v2018.01.1"
-#cmake_and_ldconfig
+clone_and_cd hackrf/host https://github.com/mossmann/hackrf.git "v2018.01.1"
+cmake_and_ldconfig
 
 # clone and build LimeSuite
-#clone_and_cd LimeSuite https://github.com/myriadrf/LimeSuite.git "v20.01.0"
-#cmake_and_ldconfig
+clone_and_cd LimeSuite https://github.com/myriadrf/LimeSuite.git "v20.01.0"
+cmake_and_ldconfig
 
 # clone and build airspyhf
-#clone_and_cd airspyhf https://github.com/airspy/airspyhf "1.1.5"
-#cmake_and_ldconfig
+clone_and_cd airspyhf https://github.com/airspy/airspyhf "1.1.5"
+cmake_and_ldconfig
 
 # clone and build Perseus
-#clone_and_cd libperseus https://github.com/f4exb/libperseus-sdr.git "afefa23e3140ac79d845acb68cf0beeb86d09028"
-#cmake_and_ldconfig
+clone_and_cd libperseus https://github.com/f4exb/libperseus-sdr.git "afefa23e3140ac79d845acb68cf0beeb86d09028"
+cmake_and_ldconfig
 
 # clone and build XTRX
-#clone_and_cd xtrx-images https://github.com/xtrx-sdr/images.git "053ec82"
-#cd ..
-#git submodule init
-#git submodule update
-#cd sources
-#mkdir build
-#cd build
-#cmake_and_ldconfig
+clone_and_cd xtrx-images https://github.com/xtrx-sdr/images.git "053ec82"
+cd ..
+git submodule init
+git submodule update
+cd sources
+mkdir build
+cd build
+cmake_and_ldconfig
 
 # clone and build airspyhf
 clone_and_cd sdrangel https://github.com/f4exb/sdrangel.git
