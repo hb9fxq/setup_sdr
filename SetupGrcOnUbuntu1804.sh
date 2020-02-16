0a023a11aebbf2717b2df4d314a5c04e37e5d03e
Frank Werner-Krippendorf <mail@hb9fxq.ch>
2020-02-16 13:32:51 +0100

    cleanup dependencies

--- SetupGrcOnUbuntu1804.sh
+++ SetupGrcOnUbuntu1804.sh
@@ -59,6 +59,10 @@ cmake_and_ldconfig(){
     make_and_ldconfig
 }
 
+apt_install_yes(){
+    apt install -y $1
+}
+
 update_and_configure_system(){ 
     update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
     update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
@@ -66,7 +70,7 @@ update_and_configure_system(){
 
     apt-get update && apt-get upgrade -y
 
-    apt install -y autoconf automake yasm build-essential ccache cmake doxygen fort77 g++ git gpsd gpsd-clients gtk2-engines-pixbuf libasound2-dev libboost-all-dev libcodec2-dev libcomedi-dev libcppunit-1.14-0 libcppunit-dev libcppunit-doc libfftw3-bin libfftw3-dev libfftw3-doc libfontconfig1-dev libgmp-dev libgmp3-dev libgps-dev libgps23 libgsl-dev libgsm1-dev liblog4cpp5-dev libncurses5 libncurses5-dbg libncurses5-dev liborc-0.4-0 liborc-0.4-dev libpulse-dev libqt5opengl5-dev libqwt-dev libqwt-qt5-dev libqwt6abi1 libqwtplot3d-qt5-dev libsdl1.2-dev libtool libudev-dev libusb-1.0-0 libusb-1.0-0-dev libusb-dev libxi-dev libxrender-dev libzmq3-dev libzmq5 ncurses-bin portaudio19-dev python-cheetah python-dev python-docutils python-gps python-gtk2 python-lxml python-mako python-numpy python-numpy-dbg python-numpy-doc python-opengl python-requests python-scipy python-setuptools python-six python-sphinx python-tk python-wxgtk3.0 python-zmq python3-click python3-click-plugins python3-gi-cairo python3-lxml python3-mako python3-numpy python3-pip python3-pyqt5 python3-scipy python3-sphinx python3-yaml python3-zmq r-base-dev swig vim wget htop curl libusb-1.0-0-dev pkg-config libfftw3-dev libqt5svg5-dev qt5-default qt5-qmake libliquid-dev python3-numpy python3-psutil python3-zmq python3-pyqt5 g++ libpython3-dev python3-pip cython3 qt5-default libfftw3-dev cmake pkg-config pkg-config autoconf automake libtool libfftw3-dev libusb-1.0-0-dev libusb-dev qt5-default qtbase5-dev qtchooser libqt5multimedia5-plugins qtmultimedia5-dev libqt5websockets5-dev qttools5-dev qttools5-dev-tools libqt5opengl5-dev qtbase5-dev libboost-all-dev libasound2-dev pulseaudio libopencv-dev libxml2-dev bison flex ffmpeg libavcodec-dev libavformat-dev libopus-dev libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev libboost-regex-dev libboost-test-dev libconfig++-dev libgmp-dev liborc-0.4-0 liborc-0.4-dev liborc-0.4-dev-bin libjsoncpp-dev libpng++-dev libvorbis-dev libxml2 libxml2-dev bison flex libaio-dev libboost-all-dev libglfw3-dev libfreetype6-dev ocl-icd-opencl-dev python3-opengl libarmadillo-dev libarmadillo-dev libi2c-dev libi2c0 libsqlite3-dev libwxgtk3.0-dev gnuplot libfltk1.3-dev pavucontrol sox libsndfile1-dev apt-transport-https ca-certificates gnupg software-properties-common wget
+    apt_install_yes "pavucontrol sox apt-transport-https ca-certificates autoconf automake libtool libudev-dev pkg-config   build-essential python3-pip libpython3-dev python-dev doxygen vim wget htop curl g++ libconfig++-dev libpng++-dev"
 
     wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -
 
@@ -85,6 +89,7 @@ update_and_configure_system(){
 update_and_configure_system
 
 # clone and build hackrf host
+apt install -y libusb-1.0-0 libusb-1.0-0-dev libusb-dev libfftw3-bin libfftw3-dev libfftw3-doc
 clone_and_cd hackrf/host https://github.com/mossmann/hackrf.git
 cmake_and_ldconfig
 
@@ -96,14 +101,17 @@ udevadm control --reload-rules
 udevadm trigger
 
 # clone and build UHD
+apt_install_yes "libboost-all-dev libncurses5 libncurses5-dbg libncurses5-dev python-numpy python-numpy-dbg python-numpy-doc python3-numpy dpdk dpdk-dev"
 clone_and_cd uhd/host https://github.com/EttusResearch/uhd "v3.15.0.0"
 cmake_and_ldconfig
 cp $SDRSOURCEDIR/script_uhd/host/utils/uhd-usrp.rules /etc/udev/rules.d/
 udevadm control --reload-rules
 udevadm trigger
 $SDRDESTDIR/lib/uhd/utils/uhd_images_downloader.py
+# TODO Thread priority scheduling
 
 # clone and build GR
+apt_install_yes "swig liblog4cpp5-dev libgmp-dev libgmp3-dev python3-pyqt5 python3-sphinx python3-yaml python3-zmq libzmq3-dev libzmq5 python3-click python3-click-plugins libsdl1.2-dev libqwt-dev libqwt-qt5-dev libqwt6abi1 libqwtplot3d-qt5-dev libgsl-dev qt5-default libqt5opengl5-dev"
 clone_and_cd gnuradio "https://github.com/gnuradio/gnuradio.git --recursive -b maint-3.8 --single-branch"
 cmake_and_ldconfig
 
@@ -115,8 +123,8 @@ cmake_and_ldconfig "-DINSTALL_UDEV_RULES=ON"
 clone_and_cd airspyone_host https://github.com/airspy/airspyone_host.git
 cmake_and_ldconfig "-DINSTALL_UDEV_RULES=ON"
 
-clone_and_cd airspyhf https://github.com/airspy/airspyhf.git
-cmake_and_ldconfig "-DINSTALL_UDEV_RULES=ON"
+#clone_and_cd airspyhf https://github.com/airspy/airspyhf.git
+#cmake_and_ldconfig "-DINSTALL_UDEV_RULES=ON"
 
 # clone and build soapySDR
 clone_and_cd SoapySDR https://github.com/pothosware/SoapySDR.git
@@ -127,6 +135,7 @@ clone_and_cd gr-osmosdr https://github.com/osmocom/gr-osmosdr.git "gr3.8"
 cmake_and_ldconfig
 
 # clone and build gqrx
+apt_install_yes "libqt5svg5-dev"
 clone_and_cd gqrx https://github.com/csete/gqrx.git
 cmake_and_ldconfig
 
@@ -135,6 +144,7 @@ clone_and_cd rtl_433 https://github.com/merbanan/rtl_433.git
 cmake_and_ldconfig
 
 # clone and build inspectrum
+apt_install_yes "libliquid-dev"
 clone_and_cd inspectrum https://github.com/miek/inspectrum
 cmake_and_ldconfig
 
@@ -143,7 +153,7 @@ clone_and_cd dump1090 https://github.com/antirez/dump1090.git
 cd .. && make && cp dump1090 $SDRDESTDIR/bin/
 
 # clone and build gr-satellites
-
+apt_install_yes "yasm"
 cd $SDRSOURCEDIR
 rm -rf scirpt_mpir_3.0.0
 wget http://mpir.org/mpir-3.0.0.zip
@@ -168,6 +178,7 @@ cmake_and_ldconfig
 # TODO: lib-fec issues https://gitlab.com/librespacefoundation/satnogs/gr-satnogs.git
 
 # clone and build gr-iio (PLUTO-SDR)
+apt_install_yes "libxml2 libxml2-dev"
 clone_and_cd libiio https://github.com/analogdevicesinc/libiio.git
 cmake_and_ldconfig
 
@@ -184,6 +195,11 @@ cmake_and_ldconfig
 clone_and_cd SoapyHackRF https://github.com/pothosware/SoapyHackRF.git
 cmake_and_ldconfig
 
+clone_and_cd SoapyAirspy https://github.com/pothosware/SoapyRTLSDR.git
+cmake_and_ldconfig
+
+read -p "Press enter to continue"
+
 clone_and_cd SoapyAirspy https://github.com/pothosware/SoapyAirspy.git
 cmake_and_ldconfig
 
@@ -191,6 +207,7 @@ clone_and_cd SoapyAirspyHF https://github.com/pothosware/SoapyAirspyHF.git
 cmake_and_ldconfig
 
 # clone and build gr-fosphor
+apt_install_yes "ocl-icd-opencl-dev python3-opengl libfreetype6-dev"
 clone_and_cd gr-fosphor https://github.com/osmocom/gr-fosphor.git
 cmake_and_ldconfig
 
@@ -215,6 +232,7 @@ clone_and_cd gr-adsb https://github.com/mhostetter/gr-adsb.git "maint-3.8"
 cmake_and_ldconfig
 
 # clone and build LimeSuite
+apt_install_yes "libsqlite3-dev libwxgtk3.0-dev gnuplot libfltk1.3-dev"
 clone_and_cd LimeSuite https://github.com/myriadrf/LimeSuite.git "stable"
 cmake_and_ldconfig
 cd ../udev-rules
@@ -233,6 +251,7 @@ ln -s $SDRDESTDIR/lib/libvolk.so /usr/lib/libvolk.so
 ln -s $SDRDESTDIR/lib/libvolk.so.2.0 /usr/lib/libvolk.so.2.0
 
 # clone and build sigutils
+apt_install_yes "libsndfile1-dev"
 clone_and_cd sigutils https://github.com/BatchDrake/sigutils.git
 cmake_and_ldconfig
 
@@ -247,7 +266,8 @@ clone_and_cd SigDigger https://github.com/BatchDrake/SigDigger
 cd .. && qmake SUWIDGETS_PREFIX=$SDRDESTDIR SIGDIGGER_PREFIX=$SDRDESTDIR SigDigger.pro
 make_and_ldconfig
 
-# urh
-pip3 install -I  urh
+# urh // TODO, fix native device support
+apt_install_yes "cython3"
+pip3 install -I  urh 
 
 chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.cache/grc_gnuradio
